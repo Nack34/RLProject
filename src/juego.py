40f576b29ac44dff307f69c35870e6ebf3f5dbfa
Nack34 <gmnack9@gmail.com>
2024-12-22 20:17:36 -0300

    add config script

--- src/juego.py
+++ src/juego.py
@@ -1,47 +1,9 @@
-import pygame, sys
-from pygame.locals import *
+import pygame, sys # type: ignore
+from pygame.locals import * # type: ignore
 from error_classes import InvalidInputError, CellOccupiedError
+from config import init_game, VERDE, ROJO, BLANCO
 
-pygame.init()
-#tamaño de la ventana
-ancho_ventana= 1080
-alto_ventana= 600
-PANTALLA = pygame.display.set_mode((ancho_ventana,alto_ventana))
-
-FPS = 60
-RELOJ = pygame.time.Clock()
-
-#Ventana
-pygame.display.set_caption("Tictactoe")
-icono = pygame.image.load("images/icons/icono_ventana.png")
-pygame.display.set_icon(icono)
-
-VERDE = (0,255,0)
-AZUL = (0,0,255)
-ROJO = (255,0,0)
-NEGRO = (0,0,0)
-BLANCO = (255,255,255)
-CELESTE = (23,227,217)
-
-
-
-#add icons to play
-img_o = pygame.image.load("images/tictactoe/o.png")
-img_x = pygame.image.load("images/tictactoe/x.png")
-
-img_o = pygame.transform.scale(img_o, (100, 100))
-img_x = pygame.transform.scale(img_x, (100, 100))
-
-
-#add & config audio
-pygame.mixer.init()
-pygame.mixer.music.load("sounds/loop.mp3")
-
-# (-1 indica bucle infinito)
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.2)
-
-
+PANTALLA, FPS, RELOJ, img_o, img_x = init_game(pygame)
 
 # variables globales
 
@@ -210,7 +172,7 @@ draw_tic_tac_toe_board()
 
 while True:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT: # type: ignore
             pygame.quit()
             sys.exit()
             pygame.mixer.music.stop()
